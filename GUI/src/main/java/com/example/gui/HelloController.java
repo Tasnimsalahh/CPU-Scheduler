e98ec6d42001a6d0b15ff7490a07c9c4f03025b2
Zainabbadr <zainabbadr32@gmail.com>
2024-04-17 00:18:27 +0200

    GUI modified2

--- GUI/src/main/java/com/example/gui/HelloController.java
+++ GUI/src/main/java/com/example/gui/HelloController.java
@@ -1,9 +1,11 @@
 package com.example.gui;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -12,10 +14,15 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.ContextMenuEvent;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
@@ -25,9 +32,28 @@ public class HelloController implements Initializable {
     private TextField NoProcesses;
     @FXML
     private Button onNext;
+    @FXML
+    private VBox squaresContainer;
+    protected static String scheduler;
 
-     protected static String scheduler;
+    @FXML
+    private AnchorPane a;
 
+    @FXML
+    private Label algorithmType;
+
+    @FXML
+    private Label turnaroundField;
+
+    @FXML
+    private Label waitingField;
+    ObservableList<Job> jobs;
+
+/*    id.setCellValueFactory(new PropertyValueFactory<jobs,Integer>("id"));
+    name.setCellValueFactory(new PropertyValueFactory<jobs,String>("name"));
+    lname.setCellValueFactory(new PropertyValueFactory<jobs,String>("lname"));
+    gmail.setCellValueFactory(new PropertyValueFactory<jobs,String>("gmail"));
+*/
     @FXML
     void getScheduler(ActionEvent event) {
          scheduler = schedulers.getValue();
@@ -45,6 +71,8 @@ public class HelloController implements Initializable {
             Scene scene = new Scene(fxmlLoader.load(), 700, 700);
             stage.setTitle("Stage 3");
             stage.setScene(scene);
+
+
         } else {
             Stage stage = (Stage) onNext.getScene().getWindow();
             FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sample1.fxml"));
@@ -54,6 +82,7 @@ public class HelloController implements Initializable {
         }
 
     }
+
 
 
     @Override
