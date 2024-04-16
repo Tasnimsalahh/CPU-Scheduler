18d9dc6d429d04e9e2444785d1a4cb7a0cd560c0
HanaSalah10 <hanasalah566@gmail.com>
2024-04-16 02:40:13 +0200

    ...

--- GUI/src/main/java/com/example/gui/HelloController.java
+++ GUI/src/main/java/com/example/gui/HelloController.java
@@ -1,10 +1,13 @@
 package com.example.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
@@ -13,17 +16,27 @@ import java.util.ResourceBundle;
 public class HelloController implements Initializable {
     @FXML
     private ComboBox<String> schedulers;
+    @FXML
+    private TextField NoProcesses;
 
     @FXML
-    void getScheduler(ContextMenuEvent event) {
-        String scheduler = schedulers.getSelectionModel().getSelectedItem();
+    void getScheduler(ActionEvent event) {
+        String scheduler = schedulers.getValue();
         System.out.println(scheduler);
 
     }
+    @FXML
+    void onNext(MouseEvent event) {
+           String num=NoProcesses.getText();
+           System.out.print(num);
+    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         schedulers.getItems().addAll("FCFS","SJF","RR","PS");
+        schedulers.setOnAction(this::getScheduler);
+
 
     }
 
